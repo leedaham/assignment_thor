e4b545bcb4750339176fd77920362e74f63a36d8
ham-tom <mydaham1004@gmail.com>
2024-01-09 16:59:12 +0900

    add @Column

--- src/main/java/me/hamtom/thor/directory/entity/Directory.java
+++ src/main/java/me/hamtom/thor/directory/entity/Directory.java
@@ -1,12 +1,44 @@
 package me.hamtom.thor.directory.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-public class Directory {
+@Getter @Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "directory")
+public class Directory extends BaseTime{
 
     @Id @GeneratedValue
+    @Column(name = "directory_id")
     private Long id;
+
+    @Column(length = 4096, nullable = false)
+    private String pathName;
+
+    @Column(nullable = false)
+    private String owner;
+
+    @Column(name = "owner_group", nullable = false)
+    private String group;
+
+    @Column(length = 9, nullable = false)
+    private String permissions;
+
+    @Column(nullable = false)
+    private int size;
+
+    public static Directory createDirectory(String pathName, String owner, String group, String permissions, int size) {
+        Directory directory = new Directory();
+        directory.setPathName(pathName);
+        directory.setOwner(owner);
+        directory.setGroup(group);
+        directory.setPermissions(permissions);
+        directory.setSize(size);
+        return directory;
+    }
+
 }
