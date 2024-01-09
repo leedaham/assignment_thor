3ed7b0c89ae64c1f62da1cc84d388caac0ebf48d
ham-tom <mydaham1004@gmail.com>
2024-01-09 16:20:36 +0900

    delete enviornment entity

--- src/main/java/me/hamtom/thor/directory/repository/EnvironmentRepository.java
+++
@@ -1,7 +0,0 @@
-package me.hamtom.thor.directory.repository;
-
-import org.hibernate.cfg.Environment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnvironmentRepository extends JpaRepository<Environment, String> {
-}
