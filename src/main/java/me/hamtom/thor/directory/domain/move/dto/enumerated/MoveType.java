217e3cdfbbb7c0b31b2483f9e6c6600b0339279f
ham-tom <mydaham1004@gmail.com>
2024-01-14 17:07:46 +0900

    move, rename 통합

--- src/main/java/me/hamtom/thor/directory/domain/move/dto/enumerated/MoveType.java
+++
@@ -1,5 +0,0 @@
-package me.hamtom.thor.directory.domain.move.dto.enumerated;
-
-public enum MoveType {
-    MOVE, MERGE
-}
--- src/main/java/me/hamtom/thor/directory/domain/rename/enumerated/RenameType.java
+++
@@ -1,5 +0,0 @@
-package me.hamtom.thor.directory.domain.rename.enumerated;
-
-public enum RenameType {
-    RENAME, MERGE
-}
