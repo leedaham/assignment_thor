6e4adbe77c05c4d847afd4fabcd19e851c465470
ham-tom <mydaham1004@gmail.com>
2024-01-14 17:09:07 +0900

    유효성 검사 예외 메시지 처리 수정

--- src/main/java/me/hamtom/thor/directory/config/ControllerExceptionHandler.java
+++ src/main/java/me/hamtom/thor/directory/config/ControllerExceptionHandler.java
@@ -8,12 +8,16 @@ import me.hamtom.thor.directory.domain.common.exception.PredictableRuntimeExcept
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.method.ParameterValidationResult;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.util.List;
 
 /**
  * 예외 발생 처리
@@ -25,24 +29,24 @@ public class ControllerExceptionHandler {
     public static final String FAIL_MSG = "요청이 실패했습니다. 다시 시도 해주세요. 계속해서 문제가 발생한다면 관리자에게 문의해주십시오.";
 
     /**
-     * HTTP Method 잘못된 경우 발생하는 Exception
-     * @param ex HttpRequestMethodNotSupportedException
-     * @return 실패 응답 (HttpStatus.BAD_REQUEST)
-     */
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<Result> handleMethodNotSupportException(HttpRequestMethodNotSupportedException ex) {
-        String message = ex.getMessage();
-        log.warn(message, ex);
-        return ResponseEntity.badRequest().body(new FailResult(message));
-    }
-
-    /**
      * 직접 정의한 Exception, 예상 가능한 예외 처리에 사용
      * @param ex 직접 정의한 Exception
      * @return 실패 응답 (HttpStatus.BAD_REQUEST)
      */
     @ExceptionHandler(PredictableRuntimeException.class)
-    public ResponseEntity<Result> handleCustomException(PredictableRuntimeException ex) {
+    public ResponseEntity<Result> handlePredictableRuntimeException(PredictableRuntimeException ex) {
+        String message = ex.getMessage();
+        log.warn(message);
+        return ResponseEntity.badRequest().body(new FailResult(message));
+    }
+
+    /**
+     * HTTP Method 잘못된 경우 발생하는 Exception
+     * @param ex HttpRequestMethodNotSupportedException
+     * @return 실패 응답 (HttpStatus.BAD_REQUEST)
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<Result> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
         String message = ex.getMessage();
         log.warn(message, ex);
         return ResponseEntity.badRequest().body(new FailResult(message));
@@ -55,7 +59,7 @@ public class ControllerExceptionHandler {
      * @return 실패 응답 (HttpStatus.BAD_REQUEST)
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Result> methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<Result> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         FieldError fieldError = ex.getBindingResult().getFieldError();
         String message = fieldError != null ? fieldError.getDefaultMessage() : "검증 실패";
         log.warn(message, ex);
@@ -63,8 +67,11 @@ public class ControllerExceptionHandler {
     }
 
     @ExceptionHandler(HandlerMethodValidationException.class)
-    public ResponseEntity<Result> handlerMethodValidationException(HandlerMethodValidationException ex) {
-        String message = ex.getMessage();
+    public ResponseEntity<Result> handleHandlerMethodValidationException(HandlerMethodValidationException ex) {
+//        String message = ex.getMessage();
+        List<ParameterValidationResult> allValidationResults = ex.getAllValidationResults();
+        ParameterValidationResult parameterValidationResult = allValidationResults.get(0);
+        String message = parameterValidationResult.getResolvableErrors().get(0).getDefaultMessage();
         log.warn(message, ex);
         return ResponseEntity.badRequest().body(new FailResult(message));
     }
@@ -75,7 +82,7 @@ public class ControllerExceptionHandler {
      * @return 실패 응답 (HttpStatus.BAD_REQUEST)
      */
     @ExceptionHandler(MissingRequestHeaderException.class)
-    public ResponseEntity<Result> handleHeaderException(MissingRequestHeaderException ex) {
+    public ResponseEntity<Result> handleMissingRequestHeaderException(MissingRequestHeaderException ex) {
         String message = ex.getMessage();
         log.warn(message, ex);
         return ResponseEntity.badRequest().body(new FailResult(message));
@@ -87,13 +94,19 @@ public class ControllerExceptionHandler {
      * @return 실패 응답 (HttpStatus.BAD_REQUEST)
      */
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<Result> handleNotReadableException(HttpMessageNotReadableException ex) {
+    public ResponseEntity<Result> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        String message = ex.getMessage();
+        log.warn(message, ex);
+        return ResponseEntity.badRequest().body(new FailResult(message));
+    }
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<Result> handleNoResourceFoundException(NoResourceFoundException ex) {
         String message = ex.getMessage();
         log.warn(message, ex);
         return ResponseEntity.badRequest().body(new FailResult(message));
     }
 
-    /**
+        /**
      * 앞에서 걸려진 예외 외의 예외로 예상하지 못한 Exception
      * @param ex Exception
      * @return 오류 응답 (HttpStatus.INTERNAL_SERVER_ERROR)
