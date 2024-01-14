d01f3e51888dc9fe2ab33e42f9e1774664264a06
ham-tom <mydaham1004@gmail.com>
2024-01-15 05:27:01 +0900

    modify optionName

--- src/main/java/me/hamtom/thor/directory/domain/create/CreateService.java
+++ src/main/java/me/hamtom/thor/directory/domain/create/CreateService.java
@@ -3,10 +3,9 @@ package me.hamtom.thor.directory.domain.create;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.hamtom.thor.directory.domain.common.DirectoryService;
-import me.hamtom.thor.directory.domain.create.dto.CreateDirectoryCommand;
-import me.hamtom.thor.directory.domain.create.dto.CreateDirectoryResult;
-import me.hamtom.thor.directory.domain.common.directory.dto.ParentDirectoriesInfoDto;
-import me.hamtom.thor.directory.domain.common.enumerated.OptionValue;
+import me.hamtom.thor.directory.domain.create.dto.CreateCommand;
+import me.hamtom.thor.directory.domain.create.dto.CreateResult;
+import me.hamtom.thor.directory.domain.common.dto.ParentDirectoriesInfoDto;
 import me.hamtom.thor.directory.domain.common.exception.PredictableRuntimeException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -20,39 +19,43 @@ import java.util.List;
 @RequiredArgsConstructor
 @Transactional
 public class CreateService {
-    @Value("${config.request.create-parents.limit}")
+    @Value("${config.policy.create-parents.limit}")
     private int createParentsLimit;
     @Value("${config.capacity}")
     private int totalCapacity;
 
     private final DirectoryService directoryService;
 
-    public CreateDirectoryResult createDirectory(CreateDirectoryCommand command) {
+    public CreateResult createDirectory(CreateCommand command) {
         //create command 가져오기
         String pathName = command.getPathName();
         String owner = command.getOwner();
         String group = command.getGroup();
         String permissions = command.getPermissions();
         int size = command.getSize();
-        OptionValue createMissingParent = command.getCreateMissingParent();
-        OptionValue flexibleCapacity = command.getFlexibleCapacity();
+        boolean isCreateMissingParent = command.isCreateMissingParent();
+        boolean isFlexibleCapacity = command.isFlexibleCapacity();
+
+        //루트 패스 확인
+        ifRootPath(pathName);
 
         //Directory 중복 확인. 중복 -> 실패 응답
         directoryService.checkAvailablePathName(pathName);
 
         //부모 디렉토리 확인
         ParentDirectoriesInfoDto parentDirectoriesInfoDto = directoryService.getParentDirectoriesInfo(pathName);
-        List<String> missingDirectories = parentDirectoriesInfoDto.getMissingDirectories();
 
         //부모 디렉터리 누락일 경우 옵션값 확인
-        boolean isCreateWithParent = ifMissingParentExist(createMissingParent, missingDirectories);
+        boolean isCreateWithParent = ifMissingParentExist(isCreateMissingParent, parentDirectoriesInfoDto);
 
         //디렉토리 남은 용량 확인, 없을 경우 실패, 부족할 경우 옵션값 확인
-        size = checkAvailableCapacityForSize(size, flexibleCapacity, missingDirectories);
+        int toCreateNum = parentDirectoriesInfoDto.countMissingParent()+1;
+        size = checkAvailableCapacityForSize(size, isFlexibleCapacity, toCreateNum);
 
         //부모 디렉토리 create
         List<String> createdParentDirectories = new ArrayList<>();
         if (isCreateWithParent) {
+            List<String> missingDirectories = parentDirectoriesInfoDto.getMissingDirectories();
             List<String> saveDirectoriesPathName = directoryService.saveDirectories(missingDirectories, owner, group, permissions, size);
             createdParentDirectories = saveDirectoriesPathName;
             log.info("부모 디렉토리 생성. pathName: {}", saveDirectoriesPathName);
@@ -62,19 +65,26 @@ public class CreateService {
         String saveDirectoryPathName = directoryService.saveDirectory(pathName, owner, group, permissions, size);
         log.info("디렉토리 생성. pathName: {}", saveDirectoryPathName);
 
-        return new CreateDirectoryResult(saveDirectoryPathName, createdParentDirectories, size);
+        return new CreateResult(saveDirectoryPathName, createdParentDirectories, size);
+    }
+
+    private void ifRootPath(String pathName) {
+        boolean isRootPath = directoryService.isRootPath(pathName);
+        if (isRootPath) {
+            throw new PredictableRuntimeException("root 경로(/)입니다. 생성할 수 없습니다.");
+        }
     }
 
 
-
-    private boolean ifMissingParentExist(OptionValue createMissingParent, List<String> missingDirectories) {
+    private boolean ifMissingParentExist(boolean isCreateMissingParent, ParentDirectoriesInfoDto parentDirectoriesInfoDto) {
         boolean isCreateWithParent = false;
-        if (!missingDirectories.isEmpty()) {
+        if (parentDirectoriesInfoDto.hasMissingParent()) {
+            List<String> missingDirectories = parentDirectoriesInfoDto.getMissingDirectories();
             log.info("부모 디렉토리 없음. missingDirectories:{}", missingDirectories);
 
             //옵션값 FALSE -> 실패 응답
-            if (createMissingParent.equals(OptionValue.FALSE)) {
-                throw new PredictableRuntimeException("부모 디렉토리가 없습니다. 부모 디렉토리와 함께 생성하길 원하실 경우 'createMissingParent=T' 옵션을 쿼리스트링으로 요청해주십시오.");
+            if (!isCreateMissingParent) {
+                throw new PredictableRuntimeException("부모 디렉토리가 없습니다. 부모 디렉토리와 함께 생성하길 원하실 경우 'createMissingParent=true' 옵션을 쿼리스트링으로 요청해주십시오.");
             }
 
             //생성해야 할 부모 디렉토리가 일정 갯수(config.yml) 초과시 실패 응답
@@ -86,7 +96,7 @@ public class CreateService {
         return isCreateWithParent;
     }
 
-    private int checkAvailableCapacityForSize(int size, OptionValue flexibleCapacity, List<String> missingDirectories) {
+    private int checkAvailableCapacityForSize(int size, boolean isFlexibleCapacity, int toCreateNum) {
         int usedCapacity = directoryService.getUsedCapacity();
         int availableCapacity = totalCapacity - usedCapacity;
 
@@ -97,24 +107,22 @@ public class CreateService {
         }
 
         //남은 용량 충분한지 확인
-        int numDirectoriesToCreate = missingDirectories.size() + 1;
-
         //남은 용량 부족
-        if (availableCapacity < (numDirectoriesToCreate * size)) {
+        if (availableCapacity < (toCreateNum * size)) {
             //flexibleCapacity 확인. FALSE -> 실패 응답
-            if (flexibleCapacity.equals(OptionValue.FALSE)) {
-                log.info("가용 용량 부족. availableCapacity: {}, numDirectoriesToCreate: {}, size {}", availableCapacity, numDirectoriesToCreate, size);
-                throw new PredictableRuntimeException("가용 용량이 부족합니다. 용량을 확보하거나, 'flexibleCapacity=T' 옵션을 쿼리스트링으로 요청해주십시오.");
+            if (!isFlexibleCapacity) {
+                log.info("가용 용량 부족. availableCapacity: {}, toCreateNum: {}, size {}", availableCapacity, toCreateNum, size);
+                throw new PredictableRuntimeException("가용 용량이 부족합니다. 용량을 확보하거나, 'flexibleCapacity=true' 옵션을 쿼리스트링으로 요청해주십시오.");
             }
 
             //flexibleCapacity 확인. TRUE
             //남은 용량을 생성할 디렉토리 수에 맞게 나누기
-            int allocatedCapacity = availableCapacity / numDirectoriesToCreate;
+            int allocatedCapacity = availableCapacity / toCreateNum;
 
             //나눈 용량이 1이 되지 않다면 실패 응답
             if (allocatedCapacity < 1) {
-                log.info("가용 용량 부족. availableCapacity: {}, numDirectoriesToCreate: {}, allocatedCapacity {}", availableCapacity, numDirectoriesToCreate, allocatedCapacity);
-                String msg = String.format("가용 용량이 부족합니다. 용량을 확보하십시오. 가용 용량: %d, 생성 디렉토리 수: %d", availableCapacity, numDirectoriesToCreate);
+                log.info("가용 용량 부족. availableCapacity: {}, toCreateNum: {}, allocatedCapacity {}", availableCapacity, toCreateNum, allocatedCapacity);
+                String msg = String.format("가용 용량이 부족합니다. 용량을 확보하십시오. 가용 용량: %d, 생성 디렉토리 수: %d", availableCapacity, toCreateNum);
                 throw new PredictableRuntimeException(msg);
             } else {
                 //나눈 용량이 1이상이라면 용량 할당
--- src/main/java/me/hamtom/thor/directory/domain/remove/RemoveService.java
+++ src/main/java/me/hamtom/thor/directory/domain/remove/RemoveService.java
@@ -3,10 +3,9 @@ package me.hamtom.thor.directory.domain.remove;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.hamtom.thor.directory.domain.common.DirectoryService;
-import me.hamtom.thor.directory.domain.common.directory.dto.ChildDirectoriesInfoDto;
-import me.hamtom.thor.directory.domain.common.enumerated.OptionValue;
+import me.hamtom.thor.directory.domain.common.dto.ChildDirectoriesInfoDto;
 import me.hamtom.thor.directory.domain.common.exception.PredictableRuntimeException;
-import me.hamtom.thor.directory.domain.remove.dto.RemoveDirectoryCommand;
+import me.hamtom.thor.directory.domain.remove.dto.RemoveCommand;
 import me.hamtom.thor.directory.domain.remove.dto.RemoveDirectoryResult;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,9 +20,9 @@ public class RemoveService {
     private final DirectoryService directoryService;
 
     @Transactional
-    public RemoveDirectoryResult removeDirectory(RemoveDirectoryCommand command) {
+    public RemoveDirectoryResult removeDirectory(RemoveCommand command) {
         String pathName = command.getPathName();
-        OptionValue removeWithChild = command.getRemoveWithChild();
+        boolean isRemoveWithChild = command.isRemoveWithChild();
 
         //디렉토리 경로 존재 확인. 존재 X -> 실패 응답
         directoryService.checkExistPathName(pathName);
@@ -33,7 +32,7 @@ public class RemoveService {
         List<String> childDirectories = childDirectoriesInfo.getChildDirectories();
 
         //자식과 함께 삭제 확인
-        boolean isDeleteWithChild = checkDeleteWithChild(removeWithChild, childDirectories);
+        boolean isDeleteWithChild = checkDeleteWithChild(isRemoveWithChild, childDirectories);
 
         //디렉토리 지우기
         if (isDeleteWithChild) {
@@ -45,11 +44,11 @@ public class RemoveService {
         return new RemoveDirectoryResult(pathName, childDirectories);
     }
 
-    private boolean checkDeleteWithChild(OptionValue removeWithChild, List<String> childDirectories) {
+    private boolean checkDeleteWithChild(boolean isRemoveWithChild, List<String> childDirectories) {
         boolean isDeleteWithChild = false;
         if (!childDirectories.isEmpty()) {
-            if (removeWithChild.equals(OptionValue.FALSE)) {
-                throw new PredictableRuntimeException("자식 디렉토리가 존재합니다. 함께 삭제하길 원하실 경우 'renameWithChild=T' 옵션을 쿼리스트링으로 요청해주십시오.");
+            if (!isRemoveWithChild) {
+                throw new PredictableRuntimeException("자식 디렉토리가 존재합니다. 함께 삭제하길 원하실 경우 'renameWithChild=true' 옵션을 쿼리스트링으로 요청해주십시오.");
             }
             isDeleteWithChild = true;
         }
